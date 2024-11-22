01df46b2c3fbae4add3bc69e61eaa95572ae43f2
xvrqt <git@xvrqt.com>
2024-11-21 18:30:01 -0800

    test

--- foot/homeManagerModule.nix
+++ foot/homeManagerModule.nix
@@ -5,7 +5,9 @@
 }: let
   # Check that the parent module is enabled, and the emulator matches
   cfgCheck = config.terminal.enable && config.terminal.emulator == "foot";
-  # Fetch themes from GitHub
+  # Get default shell
+  shell = config.terminal.shell or "bash";
+  # Fetch theme from GitHub
   catppuccinMochaTheme = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/catppuccin/foot/009cd57bd3491c65bb718a269951719f94224eb7/catppuccin-mocha.conf";
     hash = "sha256-plQ6Vge6DDLj7cBID+DRNv4b8ysadU2Lnyeemus9nx8=";
@@ -14,9 +16,19 @@ in {
   # Enable foot
   programs.foot = {
     enable = cfgCheck;
+    # Foot uses a single daemon to supply its clients
+    server.enable = cfgCheck;
+    # Only in NixOS
+    # theme = "catppuccin-mocha";
     settings = {
       main = {
+        inherit shell;
+        login-shell = "yes";
         include = "${catppuccinMochaTheme}";
+        dpi-aware = "yes";
+      };
+      mouse = {
+        hide-when-typing = "yes";
       };
     };
   };
