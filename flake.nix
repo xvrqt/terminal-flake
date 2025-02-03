ade962b1eff1cab1907ac026702dc0ab88eba7e4
xvrqt <git@xvrqt.com>
2025-02-02 20:17:14 -0800

    UGH

--- flake.nix
+++ flake.nix
@@ -5,42 +5,44 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     # Useful CLI programs
-    cli.url = "/home/crow/Development/cli-flake";
+    cli.url = "github:xvrqt/cli-flake";
     # Preconfigured NeoVim
     neovim.url = "github:xvrqt/neovim-flake";
   };
 
-  outputs = {
-    cli,
-    neovim,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
+  outputs =
+    { cli
+    , neovim
+    , nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
       # NixPkgs Setup
       lib = pkgs.lib;
-      pkgs = import nixpkgs {inherit system;};
+      pkgs = import nixpkgs { inherit system; };
 
       # Submodules used in outputs
       # Fonts to include for proper rendering of some features
-      fonts = import ./fonts.nix {inherit pkgs;};
+      fonts = import ./fonts.nix { inherit pkgs; };
       # New Config Options
-      options = import ./options.nix {inherit lib emulators;};
+      options = import ./options.nix { inherit lib emulators; };
       # Submodules required in all outputs, collated
-      required = [options fonts];
+      required = [ options fonts ];
 
       # Configuration Parameters
       # Which terminals are available to enable
-      emulators = ["alacritty" "foot"];
+      emulators = [ "alacritty" "foot" ];
 
       # Enable the config of other sub-flakes (CLI & NeoVim)
-      configure_shell = {config, ...}: {
+      configure_shell = { config, ... }: {
         # Enable the specified shell in `programs`
         programs.${config.terminal.shell}.enable = lib.mkDefault true;
         # TODO: configure the user default shell and pkgs
       };
-    in rec {
+    in
+    rec {
       homeManagerModules = {
         maximal = {
           imports =
@@ -50,7 +52,7 @@
               # Extremely customized NeoVim
               neovim.homeManagerModules.${system}.default
               # Terminal Emulator Configurations
-              (import ./homeManagerModule.nix {inherit lib emulators;})
+              (import ./homeManagerModule.nix { inherit lib emulators; })
               # Configure the sub-flakes based on terminal options
               configure_shell
             ]
